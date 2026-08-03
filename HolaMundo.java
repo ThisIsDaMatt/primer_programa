7b9459ee40727bf18dfc1c2e6420f412f7070136
Matias <matiasmunera08@gmail.com>
2026-08-03 06:40:16 -0500

    Se añaden comentarios en el codigo de java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,16 +1,23 @@
+// Se importa Scanner para ser usado como toma de datos
 import java.util.Scanner;
 
 public class HolaMundo {
     public static void main(String[] args) {
+
+        // Se abre el scanner
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Por favor, ingrese su nombre: ");
         String nombre = scanner.nextLine();
+
         System.out.println("Bienvenido, " + nombre + ", a nuestro primer programa de Java!");
         System.out.println("Desea ver la información del equipo?");
         System.out.println("1) Si");
         System.out.println("2) No, salir del programa");
+
         Integer opcion = scanner.nextInt();
         
+        // Se hace un switch para seleccionar las diferentes versiones
         switch(opcion) {
             case 1:
                 System.out.println("Bla bla bla acá va la información del equipo");
@@ -21,6 +28,8 @@ public class HolaMundo {
             default:
                 System.out.println("Por favor, ingrese una opción válida.");
             }
+
+        // Se cierra el scanner
         scanner.close();
     }
 }
